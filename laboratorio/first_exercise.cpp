9e590722675849c5ed57d22c080a55afca29c057
desireebramato-09 <desiree.bramato@salveminialessano.edu.it>
2025-09-25 12:49:28 +0200

    Update first_exercise.cpp
    aggiornamento del file "uno.cpp" con la funzione somma

---
+++ laboratorio/first_exercise.cpp
@@ -0,0 +1,13 @@
+#include <iostream.h>
+using namespace std;
+int main ()
+{
+  int num_1,num_2,somma;
+  cout<<"inserisci il primo numero: ";
+  cin>>num_1;
+  cout<<"inserisci il secondo numero: ";
+  cin>>num_2;
+  somma=num_1+num_2;
+  cout<<"la somma dei seguenti numeri e': "
+  return 0;
+}

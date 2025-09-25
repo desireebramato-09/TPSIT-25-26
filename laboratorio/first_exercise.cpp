d77e92c7cae527d48f820a1efa0731669c5e5454
desireebramato-09 <desiree.bramato@salveminialessano.edu.it>
2025-09-25 13:01:39 +0200

    Update first_exercise.cpp
    aggiornamento del file "first_exercise.cpp" con aggiunta della funzione differenza

--- laboratorio/first_exercise.cpp
+++ laboratorio/first_exercise.cpp
@@ -2,13 +2,15 @@
 using namespace std;
 int main ()
 {
-  int num_1,num_2,somma;
+  int num_1,num_2,somma,differenza;
   cout<<"inserisci il primo numero: ";
   cin>>num_1;
   cout<<"inserisci il secondo numero: ";
   cin>>num_2;
   somma=num_1+num_2;
   cout<<"la somma dei seguenti numeri e': "<< somma;
+  differenza= num_1-num_2;
+  cout<<"la differenza dei seguenti numeri e' :"<< differenza;
   
   return 0;
 }

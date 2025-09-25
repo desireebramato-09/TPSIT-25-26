0c9709af31f35774ee36d653c5768c661b8aec9b
desireebramato-09 <desiree.bramato@salveminialessano.edu.it>
2025-09-25 12:58:52 +0200

    Revert "Update first exercise.cpp"
    This reverts commit 7a1c30dc36b6500f21000fc1f50fd989f8259949.

--- laboratorio/first_exercise.cpp
+++ laboratorio/first_exercise.cpp
@@ -2,14 +2,12 @@
 using namespace std;
 int main ()
 {
-  int num_1,num_2,somma,differenza;
+  int num_1,num_2,somma;
   cout<<"inserisci il primo numero: ";
   cin>>num_1;
   cout<<"inserisci il secondo numero: ";
   cin>>num_2;
   somma=num_1+num_2;
-  cout<<"la somma dei seguenti numeri e': "<< somma;
-  differenza= num_1-num_2;
-  cout<<"la differenza dei seguenti numeri e' :"<< differenza;
+  cout<<"la somma dei seguenti numeri e': "
   return 0;
 }
